881898d455bcdd00798cd0bdfae37be2e4685ac1
Paul Graham <paulgraham3141@icloud.com>
2020-04-17 11:24:15 +0100

    update to weather info (added wind speed)

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,14 @@ const forecast = ( latitude, longitude, callback) => {
     } else if (body.error){
         callback('Unable to find location.', undefined)
     } else {
-        callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + '.' )
+        callback(undefined, body.current.weather_descriptions[0] 
+            + '. It is currently ' 
+            + body.current.temperature 
+            + ' degrees out. It feels like ' 
+            + body.current.feelslike + '.'
+            + ' Wind speed ' + body.current.wind_speed 
+            + ' Direction: ' + body.current.wind_dir )
+        //console.log(body.current)
     }
 })
 }
